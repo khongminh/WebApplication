31a503428f1aefe5e1b8d0566478189fe91652ae
mrechcheo149162536 <mrechcheo149162536@gmail.com>
2018-01-27 09:37:09 +0700

    Movie

--- MVCNetCore/Startup.cs
+++ MVCNetCore/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MVCNetCore.Models;
 
 namespace MVCNetCore
 {
@@ -22,6 +24,9 @@ namespace MVCNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
