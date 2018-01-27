ee79290ee749341951df2a99653f42aec50171a0
mrechcheo149162536 <mrechcheo149162536@gmail.com>
2018-01-27 08:59:53 +0700

    Create HelloWorld Controller

---
+++ MVCNetCore/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCNetCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult Welcome(string name, int age)
+		{
+			ViewData["name"] = name;
+			ViewData["age"] = age;
+			return View();
+		}
+    }
+}
